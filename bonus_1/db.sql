7a2e3023f64fe513cfbadea55cd256202077703e
= <=>
2021-07-15 16:42:14 +0100

    bonus 1 query add

--- bonus_1/db.sql
+++ bonus_1/db.sql
@@ -1,10 +1,11 @@
+/* First Query*/
 SELECT apt_tenant.tenant_id, tenants.tenant_name, COUNT(apt_tenant.tenant_id) AS num_of_apt
 FROM apt_tenant 
 JOIN tenants ON tenants.tenant_id = apt_tenant.tenant_id 
 GROUP BY apt_tenant.tenant_id HAVING COUNT(apt_tenant.tenant_id) > 1;
 
 
-
+/* Second Query*/
 SELECT building.building_name, COUNT(building.building_name) AS num_of_open_req
 FROM apartments
 JOIN requests ON requests.apt_id = apartments.apt_id
